0ffd7b211def774dbb871d0584c63b5cea8f5811
James <jamesballsemail@gmail.com>
2023-01-25 21:05:24 +0000

    Spotify BG Script

---
+++ bin/spotify-bg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+url=`/home/james/bin/spotify-cli art`
+
+firstString="feh --bg-tile $url"
+secondString="https://i.scdn.co/image/"
+replacement="https://open.spotify.com/image/"
+
+command="${firstString/$replacement/"$secondString"}"
+
+eval $command
